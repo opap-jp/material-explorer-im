8d15d43bc93c51f8512a5d7e642bac60bfda2c01
bromne <bromne@gmail.com>
2018-05-20 02:43:53 +0900

    テストケースを追加しました。

--- test/test_im.py
+++ test/test_im.py
@@ -1,5 +1,14 @@
 import unittest
+from os import path
 from src import im
+from io import BytesIO
+
+THUMB_LENGTH = str(290)
+
+def resolve_image(name):
+    p = path.dirname(__file__) + "/fixture/images/" + name
+    with open(p, "rb") as f:
+        return (BytesIO(f.read()), name)
 
 class ImageMagickTestCase(unittest.TestCase):
     def setUp(self):
@@ -12,5 +21,21 @@ class ImageMagickTestCase(unittest.TestCase):
         response = self.app.get("/ping")
         self.assertEqual(response.data, b"pong")
 
+    def test_resize_with_valid_params(self):
+        response = self.app.post("/resize",
+            content_type='multipart/form-data',
+            data=dict(
+                width=THUMB_LENGTH,
+                height=THUMB_LENGTH,
+                data=resolve_image("kosys.png"),
+            )
+        )
+        self.assertEqual(response.status_code, 200)
+
+    def upload(self, params):
+        return self.app.post("/resize",
+            content_type='multipart/form-data',
+            data=params
+        )
 if __name__ == "__main__":
     unittest.main()
